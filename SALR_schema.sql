9612c95e207cf3ca37ac77bff06b2a7a19061c1d
Sal-r <sal-russo2@Outlook.com>
2020-04-14 01:50:34 -0400

    table creation done?

--- SALR_schema.sql
+++ SALR_schema.sql
@@ -5,3 +5,47 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE dept_emp (
+	emp_no VARCHAR NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no VARCHAR NOT NULL,
+	from_date INT NOT NULL,
+	to_date INT NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+CREATE TABLE salaries (
+	emp_no VARCHAR NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+CREATE TABLE titles (
+	emp_no VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+
+SELECT * FROM departments;
